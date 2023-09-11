7a0f820cb499df885eadfe1b9d710ea4a7231080
HarshikaAdarsh <92363685+HarshikaAdarsh@users.noreply.github.com>
2023-09-11 16:24:45 +0530

    ceiling no completed

--- BinarySearch/Ceiling_Number.java
+++ BinarySearch/Ceiling_Number.java
@@ -2,29 +2,35 @@ package BinarySearch;
 
 public class Ceiling_Number {
     public static void main(String[] args) {
-        int[] arr = {1,3,5,6,8,10,20};
-        int target = 4;
-        int ans= ceiling_Number(arr,target);
+        int[] arr={2,3,5,9,14,16,18};
+        int target = 15;
+        int ans = ceiling(arr,target);
         System.out.println(ans);
+
     }
-    public  static int ceiling_Number(int[] arr, int target ) {
-        int start = 0;
-        int end = arr.length - 1;
-        int mid=0;
-
-        while (start <= end) {
-             mid = start + (end - start) / 2;
-
-            if (target == arr[mid]) {
-                return mid;
-            } else if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                 start=mid+1;
-            }
+    public static int ceiling(int[] arr , int target){
+        if (target > arr[arr.length - 1]) {
+          return 1;
         }
-        return arr[mid+1];
+        int start = 0 ;
+        int end= arr.length-1;
 
+        while (start <= end){
+
+            int mid = start + (end-start) /2;
+
+            if (target < arr[mid]){
+                end = mid-1;
+            }
+            else if(target>arr[mid]){
+                start = mid +1;
+            }
+            else{
+                return mid ;
+            }
+          }
+        return start;
     }
+
 
 }
