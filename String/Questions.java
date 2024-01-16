ee53c7ee2ffb9e27728876fd09bb99311815b41e
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-01-16 21:01:18 +0530

    2nd approach

--- String/Questions.java
+++ String/Questions.java
@@ -2,7 +2,9 @@ package String;
 
 public class Questions {
     public static void main(String[] args) {
-        String word = "madam";
+
+     /* 1st method
+        String word = "maam";
          word.toCharArray();
          for(int i=0;i< word.length();i++){
              for (int j=word.length() -1;j >= 0;j--){
@@ -16,5 +18,30 @@ public class Questions {
              }
          }
         System.out.println("palindrome");
+
+
+      */
+
+
+        // 2nd Method
+         String str = "abcdcba";
+        System.out.println(isPalindrome(str));
+
+    }
+    static boolean isPalindrome(String str){
+        if(str == null || str.length() == 0){
+            return true;
+        }
+        str = str.toLowerCase();
+        for(int i =0; i<=str.length()/2; i++){
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1 -i);
+
+            if (start!= end){
+                return false;
+            }
+
+        }
+        return true;
     }
 }
