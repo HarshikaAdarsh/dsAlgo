a28dd058d9b3a788c05febac16bf7898a3822a18
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-01-16 20:23:25 +0530

    palindrome

--- String/Questions.java
+++ String/Questions.java
@@ -1,4 +1,20 @@
 package String;
 
 public class Questions {
+    public static void main(String[] args) {
+        String word = "madam";
+         word.toCharArray();
+         for(int i=0;i< word.length();i++){
+             for (int j=word.length() -1;j >= 0;j--){
+                 if (word.charAt(i) == word.charAt(j) ){
+                     i++;
+
+                 }else{
+                     System.out.println("Not palindrome");
+                     return;
+                 }
+             }
+         }
+        System.out.println("palindrome");
+    }
 }
