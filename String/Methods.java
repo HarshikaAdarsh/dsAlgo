5ed56a486181e08dfb30c14e6c7b5c5a366d04fa
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-01-17 11:52:18 +0530

    methods completed

--- String/Methods.java
+++ String/Methods.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class Methods {
     public static void main(String[] args) {
-        String name = "Alex" ;
+        String name = "Alex johnson" ;
         System.out.println(Arrays.toString(name.toCharArray()));
         System.out.println(name.toLowerCase());
         System.out.println(name.toUpperCase());
         System.out.println(name.indexOf('x'));
-        System.out.println("   Alex johnson   ".strip());
+        System.out.println(" ".strip());
         System.out.println(Arrays.toString(name.split("")));
-        System.out.println(name.trim());
+
     }
 }
