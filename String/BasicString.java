7213265d3a95fc6a4f08dadbac3dd4fbab64e254
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-01-25 12:19:04 +0530

    printing

--- String/BasicString.java
+++ String/BasicString.java
@@ -1,4 +1,5 @@
 package String;
+import java.util.Scanner;
 
 public class BasicString {
 //memory management
@@ -7,6 +8,20 @@ public class BasicString {
 */
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+      // this wil only give us a single word next()
+      //  String s = scanner.next();
+        String s = scanner.nextLine();
+        // this will give the whole line till it finishes
+        System.out.println(s);
+        System.out.println(s.length());
+
+        // if you want to use double quotes within the string
+//        String sentence = "Alex is a "very" good boy"; // we cannot use it like this so
+        // we will use escape character
+        String sentence = "Alex is a \"very\" good boy";
+        //escape char = it says whatever is written next to \ do not read it just write is as it is
+        System.out.println(sentence);
         String a = "harshika";
         String b = "harshika";
         System.out.println(a==b);
--- String/Methods.java
+++ String/Methods.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Methods {
     public static void main(String[] args) {
+
         String name = "Alex johnson" ;
         System.out.println(Arrays.toString(name.toCharArray()));
         System.out.println(name.toLowerCase());
