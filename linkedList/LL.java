ab9be250f8f5fb103513746f384807871ed1ca19
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-02-19 21:09:28 +0530

    linked list

---
+++ linkedList/LL.java
@@ -0,0 +1,12 @@
+package linkedList;
+
+public class LL {
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value ;
+        }
+    }
+}
---
+++ linkedList/main.java
@@ -0,0 +1,11 @@
+package linkedList;
+
+import java.util.LinkedList;
+
+public class main {
+    public static void main (String[] args){
+        // This is how you can access inbuilt linked list
+        // LinkedList<Integer> list = new LinkedList<>();
+        // list.add(34);
+    }
+}
