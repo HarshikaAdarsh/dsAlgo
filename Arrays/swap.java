59c2b7f1045bb18b90b443b5945906581daa3079
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-03-02 15:22:35 +0530

    swap

--- Arrays/swap.java
+++ Arrays/swap.java
@@ -1,8 +1,15 @@
 package Arrays;
-
+import java.util.Arrays;
 public class swap {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7};
-        
+        SwapNumbers(arr,1,2);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void SwapNumbers(int[]arr ,int  index1 , int index2){
+        int temp ;
+        temp = arr[index1];
+        arr[index1]= arr[index2];
+        arr[index2] = temp ;
     }
 }
