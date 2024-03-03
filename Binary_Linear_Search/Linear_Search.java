23c7f92bd40c1a328c465f9b9dcc851287fe4b7f
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-03-03 18:59:38 +0530

    Update Linear_Search.java

--- Binary_Linear_Search/Linear_Search.java
+++ Binary_Linear_Search/Linear_Search.java
@@ -1,4 +1,10 @@
 package Binary_Linear_Search;
 
 public class Linear_Search {
+    public static void main(String[] args) {
+
+    }
+    public static  int LinearSearching(int n){
+        if (arr[i])
+    }
 }
