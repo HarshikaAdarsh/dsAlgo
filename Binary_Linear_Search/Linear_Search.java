bd79aceb90296a89a8274a31dd6d8ab54943a38f
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-03-04 12:59:32 +0530

    linear search

--- Binary_Linear_Search/Linear_Search.java
+++ Binary_Linear_Search/Linear_Search.java
@@ -2,9 +2,23 @@ package Binary_Linear_Search;
 
 public class Linear_Search {
     public static void main(String[] args) {
-
+        int[] arr = {2,7,4,3,7,3,4,5};
+        int ans = LinearSearching(arr,5);
+        System.out.println(ans);
     }
-    public static  int LinearSearching(int n){
-        if (arr[i])
+    public static  int LinearSearching(int[] arr, int target) {
+ // test case1 //
+       if (arr.length == 0){
+           return -1 ;
+       }
+
+ // main code //
+        for (int i = 0; i < arr.length; i++) {
+           if(arr[i] == target){
+               return i;
+           }
+        }
+  // test case2 if element do not found//
+        return -1;
     }
 }
