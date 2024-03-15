4dc3545cd7a327af5d7b8e7d4e39456ac7690e2e
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-03-15 15:24:15 +0530

    Update Binary_tree.java

--- Tree/Binary_tree.java
+++ Tree/Binary_tree.java
@@ -1,4 +1,18 @@
 package Tree;
-
+import java.util.Scanner;
 public class Binary_tree {
+   public static class Node{
+       int value;
+       Node left;
+       Node right;
+       public Node(int value){
+           this.value = value;
+       }
+   }
+   private Node root;
+
+   // insert elements
+    public void populate(Scanner scanner){
+          
+    }
 }
