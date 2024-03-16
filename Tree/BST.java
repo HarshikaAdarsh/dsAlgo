fae4d5bb89ff3a0cf742fa767c3b8004b713c270
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-03-16 17:39:21 +0530

    Create BST.java

---
+++ Tree/BST.java
@@ -0,0 +1,69 @@
+package Tree;
+
+public class BST {
+    public class Node {
+        private int value;
+        private int height;
+        private Node left;
+        private Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private Node root;
+
+    public BST() {  // Constructor
+
+    }
+
+    public int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void insert(int value) { // constructor
+
+    }
+
+    public Node insert(int value, Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+        node.height = Math.max(height(node.left), height(node.right))  +  1;
+        return node ;
+    }
+
+
+    public void display(){
+        display(root,"Root Node : ");
+    }
+    private void display(Node node, String details){
+        if(node == null){
+            return;
+        }
+        System.out.println(details + node.getValue());
+
+        display(node.left, "Left Child of" + node.getValue() + " : ");
+        display(node.right, "Right Child of" + node.getValue() + " : ");
+    }
+
+}
