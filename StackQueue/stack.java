cb6bc7d60f3ade8ab0c744000df751beb6eafbe8
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-04-01 15:41:59 +0530

    Update stack.java

--- StackQueue/stack.java
+++ StackQueue/stack.java
@@ -1,4 +1,12 @@
 package StackQueue;
-
+import java.util.Stack;
 public class stack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(45);
+        stack.push(9);
+        stack.push(18);
+
+    }
 }
