f1d2917661ddf14b47b3afe8d315bb15f1b18671
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-07-17 10:12:48 +0530

    nothing

---
+++ Binary_Linear_Search/Questions/Nothing.java
@@ -0,0 +1,8 @@
+package Binary_Linear_Search.Questions;
+
+public class Nothing {
+    public static void main(String[] args) {
+        char ch = 'a' + 2;
+        System.out.println(ch);
+    }
+}
