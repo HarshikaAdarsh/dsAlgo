2dd2c0f2aec13a5d2dcb568cc4cb1780435e0a0d
Harshika Adarsh <harshikaadarsh81@gmail.com>
2024-10-10 06:37:17 +0530

    Update CustomQueue.java

--- StackQueue/CustomQueue.java
+++ StackQueue/CustomQueue.java
@@ -1,5 +1,36 @@
 package StackQueue;
 
 public class CustomQueue {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+    int end = 0 ;
 
+    public CustomQueue(){
+        this(DEFAULT_SIZE);
+    }
+    public CustomQueue(int size){
+
+        this.data = new int[size];
+    }
+    public boolean isFull(){
+
+        return end == data.length  ;
+    }
+    private boolean isEmpty(){
+
+        return end ==  0 ;
+    }
+    public boolean insert(int item){
+        if(isFull()){
+            return false;
+        }
+        data[end++] = item; // this means first it assign the value and then do end++
+        // it is similar to
+        // data[end] = item;
+        // end++;
+
+        return true;
+    }
+
+    // removal
 }
